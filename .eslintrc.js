6ec00972647bdc0eef0bb04610a236f60ee8d2d2
oprogramador <uomodislesia@gmail.com>
2021-10-15 20:04:28 +0200

    show eslint-plugin-import not working

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: [
+    'import',
+  ],
+  rules: {
+    'import/no-unresolved': 'error',
+  },
+};
